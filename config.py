6f03804940f6195aedc664c3eeec7567b0575078
gohan5858 <kuromedaka1003@gmail.com>
2023-08-25 16:23:54 +0900

    [add] ユーザーデータの取得機能を追加

---
+++ config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+class SystemConfig:
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8".format(
+            **{
+                "user": os.getenv("USER"),
+                "password": os.getenv("PASSWORD"),
+                "host": os.getenv("HOST"),
+                "db_name": os.getenv("DB_NAME"),
+            }
+        )
+    )
+
+
+Config = SystemConfig
