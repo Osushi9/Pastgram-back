edf767c91a347f0394105980e4b0f8a841601cb3
Tatsuya Tanaka <kanatatsu64@gmail.com>
2023-09-10 01:05:31 +0900

    remove home and add hello wrld

--- api/__init__.py
+++ api/__init__.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from flask_login import LoginManager
 from api.database import db
 from api.views.auth import auth
-from api.views.home import home
 from api.views.api import api
 from api.views.page import page
 from api.views.storage import storage_router
@@ -19,13 +18,15 @@ app.config.from_object("config.Config")
 db.init_app(app)
 
 app.register_blueprint(auth, url_prefix="/auth")
-app.register_blueprint(home, url_prefix="/home")
 app.register_blueprint(page, url_prefix="/page")
 app.register_blueprint(storage_router, url_prefix="/storage")
 app.register_blueprint(api, url_prefix="/api")
 
-login_manager = LoginManager(app)
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello World!"
 
+login_manager = LoginManager(app)
 
 @login_manager.user_loader
 def load_user(user_id):
